e784ec4a7d8f525ffa968ebeabcb3a124e53637c
piyushmalik11 <sunnypiyush11@gmail.com>
2026-01-17 21:46:20 +0530

    minor changes

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GOOGLE_API_KEY = os.getenv("GEMINI_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 if not GOOGLE_API_KEY:
-    raise RuntimeError("GOOGLE_API_KEY is not set")
+    raise RuntimeError("GEMINI_API_KEY is not set")
 
 if not OPENAI_API_KEY:
     raise RuntimeError("OPENAI_API_KEY is not set")
