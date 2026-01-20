c61ea0b237805c215bb7c89496ee38190ded408b
piyushmalik11 <sunnypiyush11@gmail.com>
2026-01-20 10:20:42 +0530

    Add Navbar component with correct casing

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,12 +3,13 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
-
   return (
     <nav className="navbar">
       <div className="nav-logo">GenAI Assistant</div>
       <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
       </ul>
     </nav>
   );
